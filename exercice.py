6f4c6eab7fec8d70366a2a73ef5c14075fb43858
Anneraymond08 <90001135+Anneraymond08@users.noreply.github.com>
2021-10-17 16:18:39 -0400

    exercice fini

--- exercice.py
+++ exercice.py
@@ -20,10 +20,30 @@ def get_bill(name, data):
 
 
 def format_number(number, num_decimal_digits):
-	return ""
+	decimal_part = abs(number) % 1.0
+	whole_part = int(abs(number))
+	decimal_str = str(int(round(decimal_part * 10**num_decimal_digits)))
+	decimal_str = '.' + decimal_str + "0" * (num_decimal_digits - len(decimal_str))
+	whole_part_str = ""
+	while whole_part >= 1000:
+		digits = whole_part % 1000
+		digits_str = f" {digits :0>3}"
+		whole_part_str = digits_str + whole_part_str
+		whole_part //= 1000
+	whole_part_str = str(whole_part) + whole_part_str
+	return whole_part_str + decimal_str
 
 def get_triangle(num_rows):
-	return ""
+	border_char = "+"
+	triangle_char = "A"
+	largeur = 1 + 2 * (num_rows - 1)
+	border_row = border_char * (largeur +2)
+	result = border_row
+	for i in range(num_rows):
+		triangle_chars = triangle_char * (i*2 +1)
+		result += "\n" + f"{border_char}{triangle_chars: ^{largeur}}{border_char}"
+	result += "\n" + border_row
+	return result
 
 
 if __name__ == "__main__":
